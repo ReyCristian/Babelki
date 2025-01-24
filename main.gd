1d51708e1dd175163af6e7c648d3d32218734ab5
Marianela Sanhueza <ferchusanmar@gmail.com>
2025-01-23 22:05:55 -0300

    cofre
    aparece el cofre

--- main.gd
+++ main.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 @export var player_scene: PackedScene  
+@export var cofre_scene: PackedScene
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,9 +10,13 @@ func _ready() -> void:
 	# Posiciono al jugador (ajusta la posición)
 	var screen_size = get_viewport_rect().size
 	player.position = Vector2((screen_size.x ) / 2,  # Centro horizontal
+		screen_size.y/2 ) 
+	var cofre = cofre_scene.instantiate()
+	cofre.position= Vector2 ((screen_size.x ) / 2,  # Centro horizontal
 		screen_size.y)  # Parte inferior de la pantalla
 	# Añadimos al jugador a la escena principal
 	add_child(player)
+	add_child(cofre)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
